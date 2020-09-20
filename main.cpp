33b7a6645ab852d3800dc7deb89350f3b3ed9abb
NJL7502L <g.hana1211.tako@gmail.com>
2020-09-20 22:00:41 +0900

    cpp11の記述を使えることを確認

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
+  vector<int> v{10, 10};
   cout << "Hello! World!" << endl;
+  cout << v[0] << "\t" << v[1] << endl;
   return 0;
 }
