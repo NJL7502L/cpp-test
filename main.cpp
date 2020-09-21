9aa35e251f8e526f0ae9de98658afdcd7d7320b8
NJL7502L <g.hana1211.tako@gmail.com>
2020-09-21 17:42:35 +0900

    assignでvectorの書き換え

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
-
-  vector<vector<int>> vv{{1, 2, 3}, {4, 5, 6}};
-  cout << "Hello! World!" << endl;
-  cout << vv[1][2] << "\t" << vv[0][1] << endl;
+  int aa[5] = {1, 2, 3, 4, 5};
+  // vector<vector<int>> vv{{1, 2, 3}, {4, 5, 6}};
+  vector<int> v{9, 10, 11, 12, 13, 14, 15};
+  cout << v[0] << "\t" << v[1] << "\t" << v[2] << "\t" << v[3] << endl;
+  v.assign(&aa[0], &aa[3]);
+  cout << v[0] << "\t" << v[1] << "\t" << v[2] << "\t" << v[3] << endl;
   return 0;
 }
